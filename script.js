22a8f8c7a474d3914e0e41b195c6d51d405d4e3c
Ajay_Giri <imlearningcoding2024@gmail.com>
2025-04-07 02:10:05 +0530

    more css

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 const toggle_nav = document.getElementById("nav_mobile");
 
 function toggleMenu() {
-    console.log("toggle_working fine");
-
-    toggle_nav.classList.toggle("hidden");
+  toggle_nav.classList.toggle("hidden");
 }
+
+// Scroll-based movement setup
+function setupIntersectionObserver(element, isLTR, speed) {
+  const intersectionCallback = (entries) => {
+    const isIntersecting = entries[0].isIntersecting;
+    console.log(element, isIntersecting);
+
+    if (isIntersecting) {
+      document.addEventListener("scroll", scrollHandler);
+    } else {
+      document.removeEventListener("scroll", scrollHandler);
+    }
+  };
+
+  const intersectionObserver = new IntersectionObserver(intersectionCallback);
+  intersectionObserver.observe(element);
+
+  function scrollHandler() {
+    const translateX =
+      (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+    // if isLTR (left to right), use positive, else negative
+    element.style.transform = `translateX(${isLTR ? translateX : -translateX}px)`;
+  }
+}
+
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
