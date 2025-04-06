9d4d1f1544bee699a2ec8b849fe38716672c0799
Ajay_Giri <imlearningcoding2024@gmail.com>
2025-04-06 15:12:07 +0530

    more css

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function toggleMenu(){
-    console.log("first")
-} 
+const toggle_nav = document.getElementById("nav_mobile")
+
+function toggleMenu() {
+    console.log("toggle_working fine")
+
+    toggle_nav.classList.toggle("hidden")
+}
